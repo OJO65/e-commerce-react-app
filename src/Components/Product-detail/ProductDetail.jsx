9ae906effedfa4f5cb1c4050e60c2e1606a5e92a
Ochieng Jeffrey Odhiambo <Dmallowa@gmail.com>
2023-12-13 12:06:59 +0300

    trying to import productdata

--- src/Components/Product-detail/ProductDetail.jsx
+++ src/Components/Product-detail/ProductDetail.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import productdata from './ProductData';
+import React from "react";
+import productdata from "./ProductData";
+import "./ProductDetail.css";
 
 const ProductDetail = () => {
   return (
-    <div>
-      <productData />
+    <div className="content">
+      {productdata.map((product) => (
+        <div key={product.id} className="container">
+          <div className="products-container">
+            <div className="product">
+              <img src={product.img} alt={product.Title} />
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default ProductDetail
+export default ProductDetail;
