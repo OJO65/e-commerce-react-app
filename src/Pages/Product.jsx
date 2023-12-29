fdba1c1c8aab481c963d18c404791d2c3ed06e9f
Ochieng Jeffrey Odhiambo <Dmallowa@gmail.com>
2023-12-29 18:23:22 +0300

    functionality is failing

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,43 +1,34 @@
 import React, { useState } from "react";
 import ProductDetail from "../Components/Product-detail/ProductDetail";
+
 const Product = () => {
   const [counters, setCounters] = useState({});
   const [cartCount, setCartCount] = useState(0);
-  const [cartItems, setCartItems] = useState({});
 
-  const handleAddToCart = (productId) => {
+  const handleAddToCart = (productId, quantity = 1) => {
+    console.log("Adding to cart:", productId, quantity);
     setCounters((prevCounters) => ({
       ...prevCounters,
-      [productId]: Math.min((prevCounters[productId] || 0) + 1, 100),
+      [productId]: Math.min((prevCounters[productId] || 0) + quantity, 100),
     }));
-    setCartCount((prevCount) => prevCount + 1);
-    setCartItems((prevItems) => ({
-      ...prevItems,
-      [productId]: (prevItems[productId] || 0) + 1, 
-    }));
+    setCartCount((prevCount) => prevCount + quantity);
   };
 
   const handleRemoveFromCart = (productId) => {
+    console.log("Removing from cart:", productId);
     setCounters((prevCounters) => ({
       ...prevCounters,
       [productId]: Math.max((prevCounters[productId] || 0) - 1, 0),
     }));
     setCartCount((prevCount) => Math.max(prevCount - 1, 0));
-    setCartItems((prevItems) => {
-      const updatedItems = { ...prevItems };
-      if (updatedItems[productId] > 0) {
-        updatedItems[productId] -= 1;
-      }
-      return updatedItems;
-    });
   };
 
   return (
     <div>
       <ProductDetail
-        counters={counters}
         onAddToCart={handleAddToCart}
         onRemoveFromCart={handleRemoveFromCart}
+        cartCount={cartCount}
       />
     </div>
   );
