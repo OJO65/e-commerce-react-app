c8f8eefbc620b67b96378b53f8a3591ff7a7f0fa
Ochieng Jeffrey Odhiambo <Dmallowa@gmail.com>
2024-01-10 23:57:49 +0300

    no changes

--- src/Components/Product-detail/ProductItem.jsx
+++ src/Components/Product-detail/ProductItem.jsx
@@ -1,26 +1,40 @@
 import React from "react";
-import './ProductItem.css';
-import productdata from "./all-product";
+import PropTypes from "prop-types";
+import productdata from "./all-product"
+import "./ProductItem.css";
+
+const ProductItem = ({ id }) => {
+
+  const product = productdata.find((product) => product.id === id);
+  
+  if (!product) {
+    return ("no product is selected");
+  }
+
+  const { name, image, price, Description } = product;
 
 
-const ProductItem = ({ name, id, price, Description }) => {
   return (
     <div className="item">
       <div key={id} className="container">
         <div className="products-container">
           <div className="product">
-            <img src={productdata} alt={name} />
+            <img src={image} alt={name} />
 
             <div className="product-details">
               <h3>{name}</h3>
               <h3>{`Kshs ${price}`}</h3>
-              <h4>{Description}</h4>
+              <p>{Description}</p>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+ProductItem.propTypes = {
+  id: PropTypes.string.isRequired,
 };
 
 export default ProductItem;
