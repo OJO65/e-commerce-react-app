09f01b66b5cfe022d62d6bf1b7288703f5e47d48
OJO65 <dmallowa@gmail.com>
2024-05-01 23:29:55 +0300

    cart icon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,35 +3,34 @@ import "./Navbar.css";
 import logo from "../Assets/logo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-const Navbar = ({ cartCount, onAddToCart }) => {
+
+const Navbar = ({ cartCount }) => {
   return (
-    <>
-      <div className="navbar">
-        <Link to={"/Homepage"}>
-          <div className="nav-logo">
-            <img src={logo} alt={logo} />
+    <div className="navbar">
+      <Link to={"/Homepage"}>
+        <div className="nav-logo">
+          <img src={logo} alt="Mbao Zetu" />
+        </div>
+      </Link>
+
+      <div className="nav-login-cart">
+        <h1>Mbao Zetu</h1>
+        <FontAwesomeIcon icon={faUser} />
+        <Link to={"/login"}>
+          <div className="login">Log in</div>
+        </Link>
+        <Link to={"/cartpage"}>
+          <div id="cart">
+            <FontAwesomeIcon icon={faShoppingCart} />
+            {cartCount > 0 && (
+              <div className="nav-login-cart-count">{cartCount}</div>
+            )}
           </div>
         </Link>
-        <h1>Mbao Zetu</h1>
-        <div className="nav-login-cart">
-          <FontAwesomeIcon icon={faUser} />
-          <Link to={"/login"}>
-            <div className="login">Log in</div>
-          </Link>
-          <Link to={"/cartpage"}>
-            <div id="cart" onClick={onAddToCart}>
-              <FontAwesomeIcon icon={faShoppingCart} />
-              {cartCount > 0 && (
-                <div className="nav-login-cart-count">{cartCount}</div>
-              )}
-            </div>
-          </Link>
-        </div>
       </div>
-    </>
+    </div>
   );
 };
 
